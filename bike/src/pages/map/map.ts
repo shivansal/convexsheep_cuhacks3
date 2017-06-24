b60837cf7bb8ce393df711e940122531fc6356e0
Rithin Venkatesh <rithin23@gmail.com>
2017-06-24 12:16:00 -0700

    bike works

--- bike/src/pages/map/map.ts
+++ bike/src/pages/map/map.ts
@@ -1,41 +1,64 @@
-import { Component, ViewChild } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, CameraPosition } from '@ionic-native/google-maps';
-
 declare var google: any;
-
 @Component({
-  selector: 'page-map',
-  templateUrl: 'map.html'
+  selector: 'Map-page',
+  templateUrl: 'Map.html'
 })
 export class MapPage {
 
-  @ViewChild('map') mapElement;
-  map:any;
+    @ViewChild('map') mapElement: ElementRef;
+    @ViewChild('directionsPanel') directionsPanel: ElementRef;
+    map: any;
 
-  constructor(private googleMaps: GoogleMaps, public navCtrl: NavController, public platform: Platform) { }
+    constructor(public navCtrl: NavController) {
 
+    }
 
-  ionViewDidLoad() {
-    this.initMap();
-    //this.tmp();
-  }
+    ionViewDidLoad(){
 
- initMap(){
-    let latLng = new google.maps.LatLng(37.322998,-122.032182)
+        this.loadMap();
+        this.startNavigating();
 
-    let mapOptions ={
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-  this.map=new google.maps.Map(this.mapElement.nativeElement,mapOptions);
+    }
 
+    loadMap(){
 
- }
+        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
 
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
 
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
+    }
+
+    startNavigating(){
+
+        let directionsService = new google.maps.DirectionsService;
+        let directionsDisplay = new google.maps.DirectionsRenderer;
+
+        directionsDisplay.setMap(this.map);
+        directionsDisplay.setPanel(this.directionsPanel.nativeElement);
+
+        directionsService.route({
+            origin: '20990 valley green dr',
+            destination: 'cupertino library',
+            travelMode: google.maps.TravelMode['DRIVING']
+        }, (res, status) => {
+
+            if(status == google.maps.DirectionsStatus.OK){
+                directionsDisplay.setDirections(res);
+            } else {
+                console.warn(status);
+            }
+
+        });
+
+    }
 
 }
